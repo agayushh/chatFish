9bbd319796f6fb4aaedca7a83f7cd30f89671a8f
agayushh <agayush088@gmail.com>
2025-09-16 16:22:35 +0530

    feat: enhance LandingPage with room management and clipboard functionality

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -6,21 +6,57 @@ import { IoClipboardOutline } from "react-icons/io5";
 const LandingPage = () => {
   const [roomId, setRoomId] = useRecoilState(roomIdAtom);
   const [roomName, setRoomName] = useRecoilState(roomNameAtom);
+  const [clientRoomId, setClientRoomId] = useState("");
   const [error, setError] = useState("");
+  const [joinError, setJoinError] = useState("");
+  const [clientName, setClientName] = useState("");
   const generateRoomId = () => {
-    return Math.random().toString(36).substring(2, 10).toLocaleUpperCase();
+    return Math.random().toString(36).substring(2, 10).toUpperCase();
   };
 
-  const roomRef = useRef<HTMLInputElement>(null);
-
-  const copyToClipboard = () => {
-    if (roomRef.current) {
-      roomRef.current.select();
-      roomRef.current.setSelectionRange(0, 99999); // For mobile devices
-      document.execCommand("copy");
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(roomId || "");
       alert("Copied the room ID: " + roomId);
+    } catch (err) {
+      console.log(err);
     }
   };
+
+  const handleCreateRoom = async () => {
+    setError("");
+    let isError = false;
+    if (!roomId?.trim() && !roomName?.trim()) {
+      setError("Enter all fields");
+      isError = true;
+    } else if (!roomId?.trim()) {
+      setError("Room id is required");
+      isError = true;
+    } else if (!roomName?.trim()) {
+      setError("Room name is required");
+      isError = true;
+    }
+    if (isError) return;
+    console.log(`Room created with the roomId ${roomId} and name ${roomName}`);
+  };
+
+  const handleJoinRoom = async () => {
+    setJoinError("");
+    let hasError = false;
+    if (!clientRoomId.trim() && !clientName.trim()) {
+      hasError = true;
+      setJoinError("Need all fields to join the room");
+    } else if (!clientRoomId.trim()) {
+      hasError = true;
+      setJoinError("Need Room id to join a room");
+    } else if (!clientName.trim()) {
+      hasError = true;
+      setJoinError("Name is required to join a room");
+    }
+    if (hasError) return;
+    console.log(`Hey ${clientName} room ${clientRoomId} `);
+  };
+
   return (
     <div>
       <div className="bg-gradient-to-tl from-black via-slate-800 to-slate-900 h-screen w-screen flex items-center justify-center flex-wrap">
@@ -40,25 +76,32 @@ const LandingPage = () => {
               value={roomName || ""}
               onChange={(e) => {
                 setRoomName(e.target.value);
-                setError(e.target.value ? "" : "Room name is required");
+                setError("");
               }}
             />
-            <div className="text-red-500 text-sm mt-1">{error}</div>
           </div>
           <div className="mt-3 flex items-center gap-2 border-2 text-white rounded-lg border-white/30 p-1 focus:border-white/60 transition-all duration-300 bg-transparent">
             <input
-              ref={roomRef}
               className="bg-transparent  w-full p-2 text-white outline-none rounded-lg focus:border-white/60 transition-all duration-300"
               placeholder="Room ID"
               value={roomId || ""}
+              onChange={(e) => {
+                setError("");
+              }}
             />
             <IoClipboardOutline
               className="text-white h-10 w-5 mr-3"
               onClick={copyToClipboard}
             />
           </div>
+          <div className="text-red-500 text-sm mt-1">{error}</div>
           <div className="mt-3">
-            <button className="bg-green-500 text-black p-3 w-full rounded-lg font-semibold hover:bg-opacity-80 transition-all duration-300">
+            <button
+              className="bg-green-500 text-black p-3 w-full rounded-lg font-semibold hover:bg-opacity-80 transition-all duration-300"
+              onClick={() => {
+                handleCreateRoom();
+              }}
+            >
               Create Room
             </button>
 
@@ -80,19 +123,35 @@ const LandingPage = () => {
             <input
               className="bg-transparent border-white/30 border-2 p-3 w-full text-white outline-none rounded-lg focus:border-white/60 transition-all duration-300"
               placeholder="Room ID"
+              value={clientRoomId || ""}
+              onChange={(e) => {
+                setClientRoomId(e.target.value.toUpperCase());
+                setJoinError("");
+              }}
             />
           </div>
           <div className="mt-3">
             <input
               className="bg-transparent border-white/30 border-2 p-3 w-full text-white outline-none rounded-lg focus:border-white/60 transition-all duration-300"
               placeholder="Your Name"
+              value={clientName || ""}
+              onChange={(e) => {
+                setClientName(e.target.value);
+                setJoinError("");
+              }}
             />
           </div>
           <div className="mt-3">
-            <button className="bg-white text-black p-3 w-full rounded-lg font-semibold hover:bg-opacity-90 transition-all duration-300">
+            <button
+              className="bg-white text-black p-3 w-full rounded-lg font-semibold hover:bg-opacity-90 transition-all duration-300"
+              onClick={() => {
+                handleJoinRoom();
+              }}
+            >
               Join Room
             </button>
           </div>
+          <div className="text-red-500 text-sm mt-1">{joinError}</div>
 
           <div className="mt-5 text-white/70 text-sm italic px-2">
             Note: This is a demo project. Please do not share any personal
