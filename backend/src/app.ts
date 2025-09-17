12526fcdcb5ac6f7785f1f94bf58152db6898d92
agayushh <agayush088@gmail.com>
2025-09-17 20:12:41 +0530

    feat: add to-regex-range package with version 5.0.1
    chore: add LICENSE and README for touch package version 3.1.1
    chore: implement nodetouch CLI for touch package version 3.1.1
    feat: add touch functionality in index.js for touch package version 3.1.1
    chore: add package.json for touch package version 3.1.1
    
    chore: add release workflow for undefsafe package version 2.0.5
    chore: add jscsrc, jshintrc, and travis configuration for undefsafe package version 2.0.5
    chore: add LICENSE and README for undefsafe package version 2.0.5
    feat: implement undefsafe functionality in lib/undefsafe.js for undefsafe package version 2.0.5

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,26 @@
 import { Hono } from "hono";
 const app = new Hono();
+import { roomToSocket } from "./websocket";
+
+app.get("/", (c) => c.text("backend of the server is running"));
 
 app.get("/chat/:roomId", async (c) => {
   const roomId = c.req.param("roomId");
-  return c.json({ message: "You joined the room" });
+
+  if (!roomToSocket.has(roomId)) {
+    return c.json({ exist: false }, 404);
+  } else {
+    return c.json({ exist: true }, 200);
+  }
+});
+
+app.get("/create/:roomId", async (c) => {
+  const roomId = c.req.param("roomId");
+  if (roomToSocket.has(roomId)) {
+    return c.json({ message: "room already exists" }, 400);
+  }
+  roomToSocket.set(roomId, new Set());
+  return c.json({ roomId, message: "room created successfully" });
 });
 
 export default app;
--- backend/src/websocket.ts
+++ backend/src/websocket.ts
@@ -5,20 +5,42 @@ interface User {
   room: string;
 }
 const socketToRoom = new Map<WebSocket, string>();
-const roomToSocket = new Map<string, Set<WebSocket>>();
+export const roomToSocket = new Map<string, Set<WebSocket>>();
 export function initWebSocket(server: any) {
   const wss = new WebSocketServer({ server });
 
   // const userRoom = new Map<string, Set<WebSocket>>()
   wss.on("connection", (socket) => {
     try {
-      socket.on("message", (message: string) => {
-        const parsedMessage = JSON.parse(message);
+      socket.on("message", (message: Buffer) => {
+        const parsedMessage = JSON.parse(message.toString());
         if (!parsedMessage || !parsedMessage.type) {
           console.log("enter something mate");
         }
 
+        if (parsedMessage.type === "create") {
+          const roomName = parsedMessage.payload.roomName;
+          if (roomToSocket.has(roomName)) {
+            console.log(`room with name ${roomName} already exists`);
+            socket.send(
+              JSON.stringify({
+                type: "error",
+                message: "Room already exists",
+              })
+            );
+            return;
+          }
+          roomToSocket.set(roomName, new Set([socket]));
+          socketToRoom.set(socket, roomName);
+          socket.send(
+            JSON.stringify({ type: "created", payload: { roomName } })
+          );
+          console.log(`room with name ${roomName} created`);
+          return;
+        }
+
         if (parsedMessage.type === "join") {
+          const roomName = parsedMessage.payload.roomName;
           console.log("Welcome to the room");
           if (socketToRoom.has(socket)) {
             console.log(
@@ -26,11 +48,21 @@ export function initWebSocket(server: any) {
               socketToRoom.get(socket)
             );
           } else {
-            socketToRoom.set(socket, parsedMessage.payload.roomName);
-            if (!roomToSocket.has(parsedMessage.payload.roomName)) {
-              roomToSocket.set(parsedMessage.payload.roomName, new Set());
+            socketToRoom.set(socket, roomName);
+            if (!roomToSocket.has(roomName)) {
+              socket.send(
+                JSON.stringify({
+                  type: "error",
+                  payload: { message: "Room not found" },
+                })
+              );
+              return;
             }
-            roomToSocket.get(parsedMessage.payload.roomName)!.add(socket);
+            roomToSocket.get(roomName)!.add(socket);
+            socketToRoom.set(socket, roomName);
+            socket.send(
+              JSON.stringify({ type: "joined", payload: { roomName } })
+            );
           }
         }
         if (parsedMessage.type === "chat") {
